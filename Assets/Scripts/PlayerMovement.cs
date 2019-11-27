b8b8ee3580f5526265bdde3c80f1aea5ea1615ec
igeier <ianallengeier@gmail.com>
2019-11-27 02:24:30 -0300

    Added scripts

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidbody2D;
+    private Vector3 change;
+
+    private void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (change != Vector3.zero)
+        {
+            MoveChar();
+        }
+    }
+
+    void MoveChar()
+    {
+        myRigidbody2D.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+            );
+    }
+}
