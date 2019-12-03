ebd674b0d546e6d927247c5d4e9d8d9a48790654
igeier <ianallengeier@gmail.com>
2019-12-03 00:08:13 -0300

    UI handling, added music, debug tools

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private FillerHandler Health;
+    [SerializeField] private FillerHandler Stamina;
+    
+    
+    public float InitHealth;
+    private float CurrentHealth;
+    public float InitStamina;
+    private float currentStamina;
+
+    private void Start()
+    {
+        Health.Initialize(InitHealth, InitHealth);
+        Stamina.Initialize(InitStamina,InitStamina);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Health.MyCurrentValue -= 10;
+            Stamina.MyCurrentValue -= 5;
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Health.MyCurrentValue += 10;
+            Stamina.MyCurrentValue += 5;
+        }
+    }
+}
--- Assets/Scripts/FillerHandler.cs
+++ Assets/Scripts/FillerHandler.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FillerHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float lerpSpeed;
+    private Image content;
+    private float currentFill;
+    public float MyMaxValue { get; set; }
+    private float currentValue;
+
+    public float MyCurrentValue
     {
-        
+        get { return currentValue; }
+        set
+        {
+            if (value > MyMaxValue)
+            {
+                currentValue = MyMaxValue;
+            }
+
+            else if (value < 0)
+            { 
+                currentValue = 0;
+            }
+            else
+            {
+                currentValue = value;
+            }
+            currentFill = currentValue / MyMaxValue;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Initialize(float currentValue, float maxValue)
     {
-        
+        MyMaxValue = maxValue;
+        MyCurrentValue = currentValue;
+    }
+
+    private void Start()
+    {
+        content = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if (currentFill != content.fillAmount)
+        {
+            content.fillAmount = Mathf.Lerp(content.fillAmount, currentFill, Time.deltaTime * lerpSpeed);
+        }
     }
 }
