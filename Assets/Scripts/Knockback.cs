00f190a7586ec0da13c90e0447d159a8beb69ab8
igeier <ianallengeier@gmail.com>
2019-12-11 17:11:06 -0300

    update

---
+++ Assets/Scripts/Knockback.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    public float knockback;
+    public float knockTime;
+    
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("enemy"))
+        {
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+            if (enemy != null)
+            {
+                enemy.GetComponent<Enemy>().currenState = EnemyState.stagger;
+                Vector2 difference = enemy.transform.position - transform.position;
+                difference = difference.normalized * knockback;
+                enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(enemy));
+            }
+        }
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D enemy)
+    {
+        if (enemy != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            enemy.velocity = Vector2.zero;
+            enemy.GetComponent<Enemy>().currenState = EnemyState.idle;
+        }
+    }
+}
---
+++ Assets/Scripts/wood_enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wood_enemy : Enemy
+{
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+    private Rigidbody2D myRigidBody;
+    
+    void Start()
+    {
+        currenState = EnemyState.idle;
+        myRigidBody = GetComponent<Rigidbody2D>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+    
+    void FixedUpdate()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if (currenState == EnemyState.idle || currenState == EnemyState.walk && currenState != EnemyState.stagger && Vector3.Distance(target.position, transform.position)<= chaseRadius && Vector3.Distance(target.position,transform.position)> attackRadius)
+        {
+            Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            myRigidBody.MovePosition(temp);
+            ChangeState(EnemyState.walk);
+        }
+    }
+
+    private void ChangeState(EnemyState newstate)
+    {
+        if (currenState != newstate)
+        {
+            currenState = newstate;
+        }
+    }
+}
