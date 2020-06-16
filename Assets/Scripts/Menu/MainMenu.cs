238ce47e8d36c1f970bbe431605fe87ed2ae0c79
igeier <ianallengeier@gmail.com>
2020-06-16 18:45:31 -0300

    Fixup main menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Load next level");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OptionsMenu()
+    {
+        
+    }
+    
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Menu/Slider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class Slider : MonoBehaviour
+{
+    public AudioMixer Mixer;
+
+    public void setLevel(float slidervalue)
+    {
+        Mixer.SetFloat("MusicVolume",Mathf.Log10(slidervalue)*20);
+    }
+}
